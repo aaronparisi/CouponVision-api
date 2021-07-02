135a25edfe8c515653b045789f5b3a87c6175dcf
Aaron <parisi.aaron@gmail.com>
2021-07-02 00:06:43 +0000

    removed debugger from grocers controller

--- app/controllers/api/grocers_controller.rb
+++ app/controllers/api/grocers_controller.rb
@@ -2,7 +2,6 @@ class Api::GrocersController < ApplicationController
   def coupon_counts_by_brand
     @grocers = Grocer.coupon_counts_by_brand
     @brands = Brand.all
-    byebug
   end
 
   def coupons_by_brand
