41a07e82c5d6ffc677fabc2118fcac3632b780e1
Aaron <parisi.aaron@gmail.com>
2021-07-08 21:56:47 +0000

    name changes for routes, controllers, and jbuilder views

--- app/controllers/api/grocers_controller.rb
+++ app/controllers/api/grocers_controller.rb
@@ -1,10 +1,12 @@
 class Api::GrocersController < ApplicationController
+  # stacked bar
   def coupon_counts_by_brand
     @grocers = Grocer.coupon_counts_by_brand
     @brands = Brand.all
   end
 
-  def coupons_by_brand
+  # line
+  def active_coupons_over_time
     @grocers = Grocer.all
     @brands = Brand.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,40 +1,43 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
-    resources :users do
-      collection do
-        get '/current-user', to: 'users#userBySessionToken'
-      end
-    end
+    # resources :users do
+    #   collection do
+    #     get '/current-user', to: 'users#userBySessionToken'
+    #   end
+    # end
 
-    resources :brands do
+    # resources :brands do
       
-    end
+    # end
 
-    resources :coupon_stores do
+    # resources :coupon_stores do
       
-    end
+    # end
 
-    resources :coupons do
+    # resources :coupons do
       
-    end
+    # end
 
-    resources :grocers do
-      collection do
-        get 'coupon_counts_by_brand_per_grocer', to: 'grocers#coupon_counts_by_brand'
-        get 'coupons_by_brand_per_grocer', to: 'grocers#coupons_by_brand'
-      end
-    end
+    # resources :grocers do
+    #   collection do
+    #     get 'coupon_counts_by_brand_per_grocer', to: 'grocers#coupon_counts_by_brand'
+    #     get 'coupons_by_brand_per_grocer', to: 'grocers#coupons_by_brand'
+    #   end
+    # end
 
-    resources :products do
+    get 'coupon_counts_by_brand', to: 'grocers#coupon_counts_by_brand'
+    get 'active_coupons_over_time', to: 'grocers#active_coupons_over_time'
+
+    # resources :products do
       
-    end
+    # end
 
-    resources :stores do
-      collection do
-        get 'total_savings', to: 'stores#total_savings'
-      end
-    end
+    # resources :stores do
+    #   collection do
+    #     get 'total_savings', to: 'stores#total_savings'
+    #   end
+    # end
 
     # resources :session, only: [:create, :destroy]
     post '/session', to: 'session#create'
